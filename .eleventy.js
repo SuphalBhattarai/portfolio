8535826e49ac659ad313aa80ddd3fb7c19d3b1c6
Suphal Bhattarai <72661463+SuphalBhattarai@users.noreply.github.com>
2026-04-17 22:04:47 +0545

    build: add asset passthrough and custom domain output

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({ "src/assets/css": "css" });
+    eleventyConfig.addPassthroughCopy({ "src/assets/js": "js" });
+    eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
+    eleventyConfig.addPassthroughCopy({ "src/assets/documents": "documents" });
+    eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
     eleventyConfig.addWatchTarget("./src/assets/css/");
     eleventyConfig.addWatchTarget("./src/assets/js/");
 
